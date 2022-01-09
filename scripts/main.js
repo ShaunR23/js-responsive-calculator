d3404070a30805568fe326332faa1e975d6c3739
Shaun Radford <shaunradford@Shauns-MacBook-Pro.local>
2022-01-08 23:10:57 -0500

    Basic calculator is functional

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,8 @@
-
 const num = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 const calcScreen = document.querySelector('.calculator-screen');
 const equals = document.querySelector('.equal-sign');
 const clear = document.querySelector('.clear');
-const plusMinus = document.querySelector('.plus-minus');
-const numBtnLg = document.querySelector('number btn-lg');
-
 
 let calculation = [];
 let previousNum = [];
@@ -14,65 +10,58 @@ let currentNum = [];
 let currentOp = [];
 
 
-
-num.forEach( num => {
-    num.addEventListener('click', (e) =>{
+num.forEach(num => {
+    num.addEventListener('click', (e) => {
         //alert(num..value)
-        console.log(currentNum.push(num.value))
-        console.log(calculation.push(num.value))
-        
-        
+        currentNum.push(num.value)
+        calculation.push(num.value)
+        calcScreen.value = calculation
         console.log(calcScreen.value = num.value)
-        console.log(currentNum)
-        console.log(calculation)
-    })})
 
-operators.forEach(operators =>{
-    operators.addEventListener('click', (e) =>{
-        //alert(operator.value);
-        console.log(currentOp.push(operators.value))
-        console.log(calculation.push(operators.value))
-        console.log(currentOp)
-        console.log(calculation)
-
-        calcScreen.value = operators.value
-        
-    })})
-
-equals.addEventListener('click', (e) =>{
-    alert(equals.value)
-    // to str and .join
-    switch(currentOp){
-    case '+':
-        calcScreen.value = previousNum + currentNum
-        break;
-    case '-':
-        calcScreen.value = previousNum - currentNum
-        break;
-    case '/':
-        calcScreen.value = previousNum / currentNum
-        break;
-    case '*':
-        calcScreen.value = previousNum * currentNum
-        break;
-    default:
-        calcScreen.value = "Try again"
-        break;
-    }
-    
-
+    })
 })
 
-clear.addEventListener('click', (e) =>{
+operators.forEach(operators => {
+    operators.addEventListener('click', (e) => {
+        //alert(operator.value);
+        calculation.push(operators.value)
+        currentOp = operators.value
+        previousNum = currentNum
+        currentNum = []
+        calcScreen.value = operators.value
+
+    })
+})
+
+equals.addEventListener('click', (e) => {
+    // alert(equals.value)
+
+    switch (currentOp) {
+        case '+':
+            calcScreen.value = Number(previousNum) + Number(currentNum)
+            break;
+        case '-':
+            calcScreen.value = previousNum - currentNum
+            break;
+        case '/':
+            calcScreen.value = previousNum / currentNum
+            break;
+        case '*':
+            calcScreen.value = previousNum * currentNum
+            break;
+        default:
+            calcScreen.value = "Try again"
+            break;
+    }
+})
+
+
+
+
+clear.addEventListener('click', (e) => {
     calcScreen.value = '0'
     calculation = [];
     previousNum = [];
     currentNum = [];
     currentOp = [];
 })
-
-
-
-
-
-
